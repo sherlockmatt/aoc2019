affe783ac66dc0d49990ad3f713745d4916e51a6
sherlockmatt <matthew.w.dawson@gmail.com>
2019-12-02 18:07:59 +0000

    Day 2

--- src/puzzles/mod.rs
+++ src/puzzles/mod.rs
@@ -2,10 +2,12 @@ use failure::_core::ops::Index;
 use failure::Error;
 
 mod puzzle01;
+mod puzzle02;
 
 pub fn run(puzzle_number: usize, input: String) -> Result<Vec<String>, Error> {
-    let puzzle_functions: [&dyn Fn(String) -> Vec<String>; 1] = [
+    let puzzle_functions: [&dyn Fn(String) -> Vec<String>; 2] = [
         &puzzle01::run,
+        &puzzle02::run,
     ];
 
     ensure!(puzzle_number <= puzzle_functions.len(), "Puzzle number {} not found", puzzle_number);
